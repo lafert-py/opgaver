1aeaea1725ff6dda78923d0244ff4e0866066544
lafert-py <lafert.py@gmail.com>
2024-03-15 23:03:50 +0100

    GPT suggested using regular expressions, I didn't know about that, I'll read up on it

--- test4(V2).py
+++ test4(V2).py
@@ -1,9 +1,10 @@
 import re
 
+
 def get_name():
     name = input("What is your name? ").split()
     name = [element.capitalize() for element in name]
-   
+
     return name
 
 
@@ -13,29 +14,55 @@ def get_age():
 
 
 def check_name(name):
-    
-    #Allowed symbols . or - 
+
+    # Allowed symbols . or -
     symbols = [
-    '+', '*', '/', '=', '<', '>', '^',  
-    '(', ')', '[', ']', '{', '}', '<', '>',  
-     ',', ';', ':', '"', "'", '?', '!', 
-    '@', '#', '$', '%', '&', '_', '\\', '|', '~', '`', '^'
+        "+",
+        "*",
+        "/",
+        "=",
+        "<",
+        ">",
+        "^",
+        "(",
+        ")",
+        "[",
+        "]",
+        "{",
+        "}",
+        "<",
+        ">",
+        ",",
+        ";",
+        ":",
+        '"',
+        "'",
+        "?",
+        "!",
+        "@",
+        "#",
+        "$",
+        "%",
+        "&",
+        "_",
+        "\\",
+        "|",
+        "~",
+        "`",
+        "^",
     ]
-    
+
     try:
         for word in name:
-            if  re.search(r'\d', word):
-                raise ValueError ("The name contains numbers")
-                
+            if re.search(r"\d", word):
+                raise ValueError("The name contains numbers")
+
             elif any(elem in symbols for elem in word):
-                raise ValueError ("The name contains special symbol")
-                
+                raise ValueError("The name contains special symbol")
+
         return " ".join(name)
     except ValueError as e:
         print(e)
-   
-
-
 
 
 def check_age(age):
@@ -44,7 +71,7 @@ def check_age(age):
             age = int(age)
             if age > 100:
                 raise ValueError("You are not human :-)")
-            
+
             else:
                 return age
         else:
@@ -52,27 +79,27 @@ def check_age(age):
 
     except ValueError as e:
         print(e)
-        
-    
-def valid_data(get_data,check_data):
+
+
+def valid_data(get_data, check_data):
     for i in range(3):
         data = check_data(get_data())
         if data is not None:
             return data
-    print ('Game over')    
+    print("Game over")
     return None
 
-   
 
 def main():
     name = valid_data(get_name, check_name)
     if name is None:
         return
-    
+
     age = valid_data(get_age, check_age)
     if name is None:
         return
-       
+
     print(f"Hello, {name}. You are {age} years old.")
-   
+
+
 main()
